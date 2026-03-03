e9af169749445f34a4d1d0eabbaff26ff49f8fff
HendrywEduardo <gabriellysaless008>
2026-03-03 08:54:38 -0300

    atualizado

--- js/script_mes.js
+++ js/script_mes.js
@@ -4,41 +4,41 @@ let mes;
 
 mes = prompt("Digite um numero (1-12): ");
 
-if (mes = 1){
+if (mes == 1){
     alert("janeiro");
 }
 
-    if(mes = 2){ 
+    if(mes == 2){ 
         alert("fevereiro");
     }
-        if(mes = 3){ 
+        if(mes == 3){ 
             alert("março");
         }
-            if(mes = 4){ 
+            if(mes == 4){ 
                 alert("abril");
             }
-                if(mes = 5){ 
+                if(mes == 5){ 
                     alert("maio");
                 }
-                    if(mes = 6){ 
+                    if(mes == 6){ 
                         alert("junho");
                     }
-                        if(mes = 7){ 
+                        if(mes == 7){ 
                             alert("julho");
                         }
-                            if(mes = 8){ 
+                            if(mes == 8){ 
                                 alert("agosto");
                             }
-                                if(mes = 9){ 
+                                if(mes == 9){ 
                                     alert("setembro");
                                     }
-                                    if(mes = 10){ 
+                                    if(mes == 10){ 
                                         alert("outubro");
                                     }
-                                        if(mes = 11){ 
+                                        if(mes == 11){ 
                                             alert("novembro");
                                         }
-                                            if(mes = 12){ 
+                                            if(mes == 12){ 
                                                 alert("dezembro");
                                             }
 
