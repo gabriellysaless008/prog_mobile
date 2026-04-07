e4fcba4146c5b390541d0ef54040738256e81caf
gabriellysaless008 <gabriellysaless008>
2026-04-07 08:12:51 -0300

    fazendo o calculo de media

--- media_aritimetica/script_media.js
+++ media_aritimetica/script_media.js
@@ -1,17 +1,18 @@
 function calcular_media(){
     
-    let nota1, nota2, nota3, nota4;
+    let nota1, nota2, nota3;
     let media;
+    let nome_aluno;
 
+    nome_aluno = document.getElementById( "nome_aluno" ).value;
     nota1 = Number( document.getElementById( "nota1" ).value );
     nota2 = Number( document.getElementById( "nota2" ).value );
     nota3 = Number( document.getElementById( "nota3" ).value );
-    nota4 = Number( document.getElementById( "nota4" ).value );
+    //nota4 = Number( document.getElementById( "nota4" ).value );
 
-    media = ( nota1 + nota2 + nota3 + nota4 ) / 4;
+    media = ( nota1 + nota2 + nota3 ) / 3;
 
-    
-
-    document.getElementById( "media" ).innerHTML = " Você obteve uma media de: " + media;
-    console.log(media);
+    document.getElementById( "media" ).innerHTML = nome_aluno + " obteve uma media de: " + media;
+    console.log( nome_aluno );
+    console.log( media );
 }
