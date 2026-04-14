a9ac2c884670a0eb851c6240131788124ec159ae
gabriellysaless008 <gabriellysaless008>
2026-04-14 07:39:58 -0300

    media concluida

--- media_aritimetica/script_media.js
+++ media_aritimetica/script_media.js
@@ -1,9 +1,12 @@
 function calcular_media(){
-    
+  
+    do{   
+
     let nota1, nota2, nota3;
     let media;
     let nome_aluno, resposta;
-
+    
+    resposta = document.getElementById( "resposta" ).value;
     nome_aluno = document.getElementById( "nome_aluno" ).value;
     nota1 = Number( document.getElementById( "nota1" ).value );
     nota2 = Number( document.getElementById( "nota2" ).value );
@@ -16,9 +19,18 @@ function calcular_media(){
     console.log( nome_aluno );
     console.log( media );
 
-    if ( resposta == "S" ) {
-        console.log("Continua");
-    } else {
-        console.log("Pare");
-    }
+        if ( resposta == "S" ) {
+            console.log("Continua");
+        } else {
+            console.log("Pare");
+        }
+        
+        console.log( media );
+        document.getElementById( "nome_aluno" ).value = "";
+        document.getElementById( "nota1" ).value = "";
+        document.getElementById( "nota2" ).value = "";
+        document.getElementById( "nota3" ).value = "";
+        document.getElementById( "resposta" ).value = ""; 
+
+    } while ( resposta == "S" );
 }
