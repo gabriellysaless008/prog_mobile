9ccc9963787a59b596cd8c22ec8ec77ee36f2a1c
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-05-05 08:41:08 -0300

    vetores

---
+++ vetor/vetor.js
@@ -0,0 +1,5 @@
+let numeros = [ 54, 21, 100, 89, 90, 32, 23, 74 ];
+
+for ( let x = 0; x <= 7; x++ ) {
+    console.log(numeros[x]);
+}
---
+++ vetor/vetor_nomes.js
@@ -0,0 +1,6 @@
+let nomes = [];
+
+for ( let x = 0; x <= 8; x++ ) {
+  
+    nomes [ x ] = prompt ( "Digite o nome do aluno:  " );
+}
